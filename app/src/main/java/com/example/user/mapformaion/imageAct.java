a8e5a0a5ff9c3e850d8cb9927e3b1205615ac88e
computer\user <mlka444@naver.com>
2017-11-10 21:29:16 +0900

    imageview 수정, 로고 추가

--- app/src/main/java/com/example/user/mapformaion/imageAct.java
+++ app/src/main/java/com/example/user/mapformaion/imageAct.java
@@ -1,14 +1,13 @@
 package com.example.user.mapformaion;
 
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 
 public class imageAct extends AppCompatActivity {
-String iid;
-android.widget.ImageView ImageView;
-
 
 
     /** Called when the activity is first created. */
@@ -17,27 +16,24 @@ android.widget.ImageView ImageView;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.imageview);
 
-        ImageView = (android.widget.ImageView)findViewById(R.id.imageView);
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        imageView.setBackgroundResource(R.drawable.frame_anim);
+        AnimationDrawable countdownAnim = (AnimationDrawable) imageView.getBackground();
+        countdownAnim.start();
 
 
+    new Thread(){
+        @Override
+        public void run(){
+            try {
+                Thread.sleep(2000);
+            finish();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
- /*       Intent intent = getIntent();
-        iid = intent.getStringExtra("image");
-
-
-
-        ImageLoaderTask imageLoad = new ImageLoaderTask(
-                ImageView,
-                iid
-        );
-        imageLoad.execute();*/
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
-        onBackPressed();
-
+    }.start();
 
     }
 
